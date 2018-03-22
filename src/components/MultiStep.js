13ce11ed21c86c073696f38f80a4c196a299c92c
Tina <tina.fan@live.com>
2018-03-22 15:22:48 -0700

    Add all fields and pages

--- src/components/MultiStep.js
+++ src/components/MultiStep.js
@@ -11,7 +11,7 @@ const Error = ({ name }) => (
   <Field
     name={name}
     render={({ form: { touched, errors } }) =>
-      touched[name] && errors[name] ? <span>{errors[name]}</span> : null
+      touched[name] && errors[name] ? <span className ="errors">{errors[name]}</span> : null
     }
   />
 );
@@ -24,6 +24,9 @@ class Wizard extends React.Component {
     this.state = {
       page: 0,
       values: props.initialValues,
+    };
+    this.fields = {
+
     };
   }
 
@@ -71,16 +74,16 @@ class Wizard extends React.Component {
         render={({ values, handleSubmit, isSubmitting, handleReset }) => (
           <form onSubmit={handleSubmit}>
             {activePage}
-            <div className="buttons">
+            <div className="action-button">
               {page > 0 && (
-                <button type="button" onClick={this.previous}>
+                <button className ="previous-button" type="button" onClick={this.previous}>
                   « Previous
                 </button>
               )}
 
-              {!isLastPage && <button type="submit">Next »</button>}
+              {!isLastPage && <button className ="next-button" type="submit">Next »</button>}
               {isLastPage && (
-                <button type="submit" disabled={isSubmitting}>
+                <button className ="submit action-button" type="submit" disabled={isSubmitting}>
                   Submit
                 </button>
               )}
@@ -111,59 +114,274 @@ const Multi = () => (
     >
       <Wizard.Page>
         <div>
-          <label>First Name</label>
+          <label htmlFor="id" style={{ display: 'block' }}>
+            ID
+            <span className="required">*</span>
+          </label>
           <Field
-            name="firstName"
             component="input"
             type="text"
-            placeholder="First Name"
+            name="id"
+            placeholder= "Sample ID"
             validate={required}
           />
-          <Error name="firstName" />
+          <Error name="id" />
         </div>
         <div>
-          <label>Last Name</label>
+          <label htmlFor="user" style={{ display: 'block' }}>
+            User
+          </label>
           <Field
-            name="lastName"
             component="input"
+            name="user"
             type="text"
-            placeholder="Last Name"
-            validate={required}
           />
-          <Error name="lastName" />
+          <Error name="user" />
+        </div>
+        <div>
+          <label htmlFor="date" style={{ display: 'block' }}>
+            Date
+          </label>
+          <Field
+            component="input"
+            type="date"
+            name="date"
+          />
+          <Error name="date" />
+        </div>
+        <div>
+          <label htmlFor="dateModified" style={{ display: 'block' }}>
+            Date Modified
+          </label>
+          <Field
+            component="input"
+            type="date"
+            name="dateModified"
+          />
+          <Error name="dateModified" />
         </div>
       </Wizard.Page>
-      <Wizard.Page
-        validate={values => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          }
-          if (!values.favoriteColor) {
-            errors.favoriteColor = 'Required';
-          }
-          return errors;
-        }}
-      >
+
+      <Wizard.Page>
         <div>
-          <label>Email</label>
+          <label htmlFor="assetName" style={{ display: 'block' }}>
+            Asset Name
+          </label>
           <Field
-            name="email"
             component="input"
-            type="email"
-            placeholder="Email"
+            type="text"
+            name="assetName"
+            validate={required}
           />
-          <Error name="email" />
+          <Error name="assetName" />
         </div>
         <div>
-          <label>Favorite Color</label>
-          <Field name="favoriteColor" component="select">
-            <option />
-            <option value="#ff0000">❤️ Red</option>
-            <option value="#00ff00">💚 Green</option>
-            <option value="#0000ff">💙 Blue</option>
-          </Field>
-          <Error name="favoriteColor" />
+          <label htmlFor="assetModelNumber" style={{ display: 'block' }}>
+            Asset Model Number
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="assetModelNumber"
+          />
+          <Error name="assetModelNumber" />
+        </div>
+        <div>
+          <label htmlFor="assetSpecs" style={{ display: 'block' }}>
+            Asset Specs
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="assetSpecs"
+          />
+          <Error name="assetSpecs" />
+        </div>
+        <div>
+          <label htmlFor="assetSerialNumber" style={{ display: 'block' }}>
+            Asset Serial Number
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="assetSerialNumber"
+          />
+          <Error name="assetSerialNumber" />
+        </div>
+        <div>
+          <label htmlFor="assetTag" style={{ display: 'block' }}>
+            Asset Tag
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="assetTag"
+          />
+          <Error name="assetTag" />
+        </div>
+        <div>
+          <label htmlFor="assetOwner" style={{ display: 'block' }}>
+            Asset Owner
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="assetOwner"
+          />
+          <Error name="assetOwner" />
+        </div>
+      </Wizard.Page>
+      
+      <Wizard.Page>
+        <div>
+          <label htmlFor="modelYear" style={{ display: 'block' }}>
+            Model Year
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="modelYear"
+            placeholder= "e.g. 1995"
+          />
+          <Error name="modelYear" />
+        </div>
+        <div>
+          <label htmlFor="speedChart" style={{ display: 'block' }}>
+            Speed Chart
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="speedChart"
+          />
+          <Error name="speedChart" />
+        </div>
+        <div>
+          <label htmlFor="vendor" style={{ display: 'block' }}>
+            Vendor
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="vendor"
+          />
+          <Error name="vendor" />
+        </div>
+        <div>
+          <label htmlFor="jvNumber" style={{ display: 'block' }}>
+            JV Number 
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="jvNumber"
+          />
+          <Error name="jvNumber" />
+        </div>
+        <div>
+          <label htmlFor="datePurchased" style={{ display: 'block' }}>
+            Date Purchased
+          </label>
+          <Field
+            component="input"
+            type="date"
+            name="datePurchased"
+          />
+          <Error name="datePurchased" />
+        </div>
+      </Wizard.Page>
+
+      <Wizard.Page>
+        <div>
+          <label htmlFor="currentUser" style={{ display: 'block' }}>
+            Current User
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="currentUser"
+          />
+          <Error name="currentUser" />
+        </div>
+        <div>
+          <label htmlFor="previousUser" style={{ display: 'block' }}>
+            Previous User
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="previousUser"
+          />
+          <Error name="previousUser" />
+        </div>
+        <div>
+          <label htmlFor="assetLocation" style={{ display: 'block' }}>
+            Asset Location
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="assetLocation"
+          />
+          <Error name="assetLocation" />
+        </div>
+        <div>
+          <label htmlFor="disposalDate" style={{ display: 'block' }}>
+            Disposal Date
+          </label>
+          <Field
+            component="input"
+            type="date"
+            name="disposalDate"
+          />
+          <Error name="disposalDate" />
+        </div>
+        <div>
+          <label htmlFor="methodOfDisposal" style={{ display: 'block' }}>
+            Method Of Disposal
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="methodOfDisposal"
+          />
+          <Error name="methodOfDisposal" />
+        </div>
+      </Wizard.Page>
+
+      <Wizard.Page>
+        <div>
+          <label htmlFor="userType" style={{ display: 'block' }}>
+            User Type
+          </label>
+          <Field
+            component="input"
+            type="text"
+            name="userType"
+          />
+          <Error name="userType" />
+        </div>
+        <div>
+          <label htmlFor="unitAffiliation" style={{ display: 'block' }}>
+            Unit Affiliation
+          </label>
+          <Field 
+            component="input"
+            type="text"
+            name="unitAffiliation"
+          />
+          <Error name="unitAffiliation" />
+        </div>
+        <div>
+          <label htmlFor="cost" style={{ display: 'block' }}>
+            Cost
+          </label>
+          <Field 
+            component="input"
+            type="number"
+            name="cost"
+          />
+          <Error name="cost" />
         </div>
       </Wizard.Page>
     </Wizard>
